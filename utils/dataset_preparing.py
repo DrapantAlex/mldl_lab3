267cfdca97c21a5a8aae673818c09d4efc3d5134
ALEX DRAPANT s297441 <s297441@studenti.polito.it>
2025-11-01 18:13:54 +0100

    Last path update (real)

--- utils/dataset_preparing.py
+++ utils/dataset_preparing.py
@@ -5,17 +5,17 @@ import zipfile
 
 def prepare_tiny_imagenet(data_dir="dataset/tiny-imagenet-200"):
     url = "http://cs231n.stanford.edu/tiny-imagenet-200.zip"
-    zip_path = os.path.join("dataset/tiny-imagenet", "tiny-imagenet-200.zip")
+    zip_path = os.path.join("dataset", "tiny-imagenet-200.zip")
 
     if not os.path.exists(data_dir):
-        os.makedirs("dataset/tiny-imagenet", exist_ok=True)
+        os.makedirs("dataset", exist_ok=True)
         urllib.request.urlretrieve(url, zip_path)
         with zipfile.ZipFile(zip_path, 'r') as zip_ref:
             zip_ref.extractall("dataset")
 
-    with open('dataset/tiny-imagenet/tiny-imagenet-200/val/val_annotations.txt') as f:
+    with open('dataset/tiny-imagenet-200/val/val_annotations.txt') as f:
         for line in f:
             fn, cls, *_ = line.split('\t')
-            os.makedirs(f'dataset/tiny-imagenet/tiny-imagenet-200/val/{cls}', exist_ok=True)
-            shutil.copyfile(f'dataset/tiny-imagenet/tiny-imagenet-200/val/images/{fn}', f'dataset/tiny-imagenet/tiny-imagenet-200/val/{cls}/{fn}')
-    shutil.rmtree('dataset/tiny-imagenet/tiny-imagenet-200/val/images')
+            os.makedirs(f'dataset/tiny-imagenet-200/val/{cls}', exist_ok=True)
+            shutil.copyfile(f'dataset/tiny-imagenet-200/val/images/{fn}', f'dataset/tiny-imagenet-200/val/{cls}/{fn}')
+    shutil.rmtree('dataset/tiny-imagenet-200/val/images')
