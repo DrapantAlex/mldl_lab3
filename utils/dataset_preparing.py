bbed0515ae87f69dc388aef91e8e0cdcb710c5f9
ALEX DRAPANT s297441 <s297441@studenti.polito.it>
2025-11-01 18:05:19 +0100

    Other path update

--- utils/dataset_preparing.py
+++ utils/dataset_preparing.py
@@ -17,5 +17,5 @@ def prepare_tiny_imagenet(data_dir="dataset/tiny-imagenet-200"):
         for line in f:
             fn, cls, *_ = line.split('\t')
             os.makedirs(f'dataset/tiny-imagenet/tiny-imagenet-200/val/{cls}', exist_ok=True)
-            shutil.copyfile(f'datset/tiny-imagenet/tiny-imagenet-200/val/images/{fn}', f'dataset/tiny-imagenet/tiny-imagenet-200/val/{cls}/{fn}')
+            shutil.copyfile(f'dataset/tiny-imagenet/tiny-imagenet-200/val/images/{fn}', f'dataset/tiny-imagenet/tiny-imagenet-200/val/{cls}/{fn}')
     shutil.rmtree('dataset/tiny-imagenet/tiny-imagenet-200/val/images')
