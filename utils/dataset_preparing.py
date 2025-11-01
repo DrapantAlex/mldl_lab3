cc0f8275caad79d1807bf5035edeccbf899555ad
ALEX DRAPANT s297441 <s297441@studenti.polito.it>
2025-11-01 18:07:41 +0100

    Last path update

--- utils/dataset_preparing.py
+++ utils/dataset_preparing.py
@@ -8,7 +8,7 @@ def prepare_tiny_imagenet(data_dir="dataset/tiny-imagenet-200"):
     zip_path = os.path.join("dataset/tiny-imagenet", "tiny-imagenet-200.zip")
 
     if not os.path.exists(data_dir):
-        os.makedirs("dataset", exist_ok=True)
+        os.makedirs("dataset/tiny-imagenet", exist_ok=True)
         urllib.request.urlretrieve(url, zip_path)
         with zipfile.ZipFile(zip_path, 'r') as zip_ref:
             zip_ref.extractall("dataset")
