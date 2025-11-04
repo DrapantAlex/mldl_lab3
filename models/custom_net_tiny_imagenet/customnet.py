fddd410df299365a4969a93b605bf9dca2ca5b9b
ALEX DRAPANT s297441 <s297441@studenti.polito.it>
2025-11-04 18:24:01 +0100

    BatchNorm update

--- models/custom_net_tiny_imagenet/customnet.py
+++ models/custom_net_tiny_imagenet/customnet.py
@@ -5,12 +5,15 @@ class CustomNet(nn.Module):
         super(CustomNet, self).__init__()
         self.features = nn.Sequential(
             nn.Conv2d(3, 64, kernel_size=3, padding=1, stride=1),
+            nn.BatchNorm2d(64),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2),
             nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2),
             nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2),
             nn.AdaptiveAvgPool2d(1)
